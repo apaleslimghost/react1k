cfac5e01b597c37589ddaf60d0d23d87cd4131c1
Matt Brennan <matt@153.io>
2016-12-20 22:59:11 +0000

    add willmount

--- react1k.js
+++ react1k.js
@@ -7,16 +7,18 @@ let createNode = ({tagName, props, data}) =>
 let modifyElement = (el, host, index) => {
 	if(typeof el === 'string') return {tagName: '_string', data: el, children: []}
 	if(typeof el.tagName === 'function') {
-		let run = props => modifyElement(el.tagName(props, nextProps => render(run(Object.assign(el.props, nextProps)), host, index)), host, index);
+		el.setProps = nextProps => render(run(Object.assign(el.props, nextProps)), host, index);
+		let run = props => modifyElement(el.tagName(props, el.setProps), host, index);
 		return run(Object.assign({children: el.children}, el.props))
 	}
 	return el;
 }
 
-let render = exports.render = (el, host, index = 0) => {
-	el = modifyElement(el, host, index)
+let render = exports.render = (origEl, host, index = 0) => {
+	el = modifyElement(origEl, host, index)
 	let extantChild = host.childNodes[index]
 	let node = createNode(el)
+	if(origEl.tagName && origEl.tagName.willMount) origEl.tagName.willMount.call(origEl, node);
 	el.children.forEach((child, i) => child && render(child, extantChild || node, i))
 	if(!extantChild) return host.append(node)
 	if(el.tagName.toUpperCase() === extantChild.tagName) return Object.assign(extantChild, el.props)
