bde8e799099ffba278ba422aa6cf44201b1f6aed
Matt Brennan <mattyb1000@gmail.com>
2016-12-20 17:50:23 +0000

    modify elements returned from components

--- react1k.js
+++ react1k.js
@@ -7,7 +7,7 @@ let createNode = ({tagName, props, data}) =>
 let modifyElement = (el, host, index) => {
 	if(typeof el === 'string') return {tagName: '_string', data: el, children: []}
 	if(typeof el.tagName === 'function') {
-		let run = props => el.tagName(props, nextProps => render(run(Object.assign(el.props, nextProps)), host, index))
+		let run = props => modifyElement(el.tagName(props, nextProps => render(run(Object.assign(el.props, nextProps)), host, index)));
 		return run(Object.assign({children: el.children}, el.props))
 	}
 }
@@ -16,7 +16,7 @@ let render = exports.render = (el, host, index = 0) => {
 	el = modifyElement(el, host, index) || el
 	let extantChild = host.childNodes[index]
 	let node = createNode(el)
-	el.children.forEach((child, i) => render(child, extantChild || node, i))
+	el.children.forEach((child, i) => child && render(child, extantChild || node, i))
 	if(!extantChild) return host.append(node)
 	if(el.tagName.toUpperCase() === extantChild.tagName) return Object.assign(extantChild, el.props)
 	extantChild.replaceWith(node)
