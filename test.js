0df184b9e9973f28263cf93fea61669149ed3f41
Matt Brennan <matt@153.io>
2017-03-19 18:50:20 +0000

    switch to babel, add initial failing test

---
+++ test.js
@@ -0,0 +1,31 @@
+/** @jsx h */
+
+const {expect} = require('chai')
+	.use(require('sinon-chai'))
+	.use(require('dirty-chai'));
+
+const {spy} = require('sinon');
+
+const promisify = require('@quarterto/promisify');
+const jsdom = promisify(require('jsdom').env);
+const {h, render} = require('./');
+
+describe('react1k', () => {
+	before(async () => {
+		const {document} = await jsdom('<main></main>');
+		Object.assign(global, {document});
+	});
+
+	it('should call willMount on a parent of a direct child', () => {
+		const Foo = () => <div />;
+		const Bar = () => <Foo />;
+
+		Foo.willMount = spy();
+		Bar.willMount = spy();
+
+		render(<Bar />, document.querySelector('main'));
+
+		expect(Foo.willMount).to.have.been.called();
+		expect(Bar.willMount).to.have.been.called();
+	});
+});
