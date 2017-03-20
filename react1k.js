b384e469a33e879aae9731ce708c43be66eb79ac
Matt Brennan <matt@153.io>
2017-03-20 21:47:22 +0000

    the wrong thing

--- react1k.js
+++ react1k.js
@@ -17,6 +17,6 @@ let render = exports.render = (el, host, index = 0) => {
 	let node = mount(el, host, index)
 	el.children && el.children.forEach((child, i) => child && render(child, extantChild || node, i))
 	if(!extantChild) return host.appendChild(node)
-	if(el.tagName.toUpperCase() === extantChild.type) return Object.assign(extantChild, el.props)
+	if(el.type.toUpperCase() === extantChild.tagName) return Object.assign(extantChild, el.props)
 	extantChild.replaceWith(node)
 }
