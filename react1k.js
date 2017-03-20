2cda6c2c7efab2dc82a497d28ec3b22f1512e70a
Matt Brennan <matt@153.io>
2017-03-20 21:40:11 +0000

    rename tagName to type for parity with react

--- react1k.js
+++ react1k.js
@@ -1,13 +1,13 @@
-exports.h = (tagName, props = {}, ...children) => ({tagName, props, children})
+exports.h = (type, props = {}, ...children) => ({type, props, children})
 
 let mount = (el, host, index) => {
 	if(typeof el === 'string') return document.createTextNode(el)
-	if(typeof el.tagName === 'string') return Object.assign(document.createElement(el.tagName), el.props)
-	if(typeof el.tagName === 'function') {
+	if(typeof el.type === 'string') return Object.assign(document.createElement(el.type), el.props)
+	if(typeof el.type === 'function') {
 		el.setProps = nextProps => render(run(Object.assign(el.props, nextProps)), host, index)
-		let run = props => mount(el.tagName(props, el.setProps), host, index)
+		let run = props => mount(el.type(props, el.setProps), host, index)
 		let node = run(Object.assign({children: el.children}, el.props))
-		if(el.tagName.willMount) el.tagName.willMount(el, node)
+		if(el.type.willMount) el.type.willMount(el, node)
 		return node
 	}
 }
@@ -17,6 +17,6 @@ let render = exports.render = (el, host, index = 0) => {
 	let node = mount(el, host, index)
 	el.children && el.children.forEach((child, i) => child && render(child, extantChild || node, i))
 	if(!extantChild) return host.appendChild(node)
-	if(el.tagName.toUpperCase() === extantChild.tagName) return Object.assign(extantChild, el.props)
+	if(el.tagName.toUpperCase() === extantChild.type) return Object.assign(extantChild, el.props)
 	extantChild.replaceWith(node)
 }
