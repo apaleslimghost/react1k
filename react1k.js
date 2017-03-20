9b23fbb2b54c6aa609b8a34fa9e7c99964a88147
Matt Brennan <matt@153.io>
2017-03-20 22:02:21 +0000

    consistency

--- react1k.js
+++ react1k.js
@@ -15,7 +15,7 @@ let mount = (el, host, index) => {
 let render = exports.render = (el, host, index = 0) => {
 	let extantChild = host.childNodes[index]
 	let node = mount(el, host, index)
-	el.children && el.children.forEach((child, i) => child && render(child, extantChild || node, i))
+	if(el.children) el.children.forEach((child, i) => child && render(child, extantChild || node, i))
 	if(!extantChild) return host.appendChild(node)
 	if(el.type.toUpperCase() === extantChild.tagName) return Object.assign(extantChild, el.props)
 }
